1cfcdde9938e1d751e4a7d2201e97414a7775169
Adil Bendaoui <90851562+Abendaoui@users.noreply.github.com>
2023-01-25 21:48:25 +0100

    Main File

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/good',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '437274e8b9d401dff3c35941f3a0c332b32f9ec3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/good' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '437274e8b9d401dff3c35941f3a0c332b32f9ec3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
